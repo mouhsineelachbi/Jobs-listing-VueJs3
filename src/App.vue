f280108448a5375bc897797625dc5ce118f256ca
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-22 20:50:49 +0100

    programatic navigation

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,60 @@
     <router-link :to="{ name: 'About'}">About</router-link> <!-- we can bind It to an object -->
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view/>
 </template>
 
+<script>
+export default {
+  methods: {
+     redirect (){
+       this.$router.push({ name: 'Home' })
+     },
+     back () {
+       this.$router.go(-1) // to go back in history
+     },
+     forward () {
+       this.$router.go(1) // to go forward in history
+     }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 30px;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none;
-  padding: 10px;
-  border-radius: 4px;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+    padding: 10px;
+    border-radius: 4px;
+  }
 
-#nav a.router-link-exact-active {
-  color: white;
-  background: crimson;
-}
+  #nav a.router-link-exact-active {
+    color: white;
+    background: crimson;
+  }
+
+  button {
+    margin: 0 10px;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+  }
 </style>
