471a460bee53e2e0ec5c3a3c22b0c03e46973934
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-22 17:15:08 +0100

    Vue router manipulation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe enim ex labore incidunt vero itaque. Ratione veritatis corrupti obcaecati sapiente. Ea dolorum dicta, neque, inventore pariatur atque labore fuga impedit quibusdam delectus laborum quas aspernatur ipsam itaque dolorem, harum eveniet consequatur debitis unde facere ducimus sapiente maiores? Impedit voluptate, quas sapiente perspiciatis tenetur molestiae enim assumenda, vitae temporibus obcaecati architecto? Minus quasi veniam asperiores neque ratione suscipit expedita, reprehenderit placeat enim repudiandae excepturi beatae molestiae. Eveniet ex eum eaque nam temporibus saepe, sequi vitae. Deserunt itaque mollitia est sint, quisquam sed quasi animi minus iusto fugit laboriosam nam ipsa magni!</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quod soluta impedit! Nostrum nobis eius minima id reiciendis ipsam culpa voluptate doloremque. Reprehenderit eveniet aperiam ex nihil. Commodi ea quaerat vitae eius magnam molestiae, similique itaque? Eos ratione necessitatibus facere quas saepe quam odio quo reiciendis quod, sapiente nemo pariatur at! Ex odit totam minima suscipit aliquam recusandae voluptate doloremque omnis, molestias laborum nisi repellat aut provident aperiam distinctio quia et iure ad tempora, quisquam nihil exercitationem nam maiores quae. Perferendis, expedita maiores, illum non voluptatem tempora ad excepturi veniam amet accusamus explicabo, quaerat cumque placeat sapiente animi quis laudantium.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores molestiae ab culpa quae saepe eius aperiam unde nobis cupiditate amet quidem quam, repellat tempore, recusandae perferendis labore vero repudiandae esse a autem odio modi! Ex consequuntur temporibus est voluptatibus rem accusamus modi nemo perspiciatis eaque natus? Consectetur eum beatae autem nesciunt quasi ea quaerat, sit eligendi perferendis itaque cupiditate voluptate praesentium vero, dolores natus pariatur veniam! Eius sunt aut voluptate dolorum autem, ratione dolore fugit architecto quam corrupti culpa qui nobis iste obcaecati ipsam impedit ut dicta ipsum! Optio maxime minima blanditiis, nam consequatur voluptate natus ea sed expedita.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
